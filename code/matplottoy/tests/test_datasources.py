77f2c92b3f64d3b6e03a829f5267522df84946e5
hannah <story645@gmail.com>
2020-07-17 18:29:10 -0400

    bar

--- code/matplottoy/tests/test_datasources.py
+++ code/matplottoy/tests/test_datasources.py
@@ -23,12 +23,13 @@ class TestArray:
     def data(self):
         array =  np.tile(np.arange(13),10).reshape(10,13)
         self.datasource = da.DataSourceArray(array)
+        self.rows, self.cols = self.datasource.data.shape
 
     @check_figures_equal(extensions=['png'])
     def test_image(self, fig_test, fig_ref):
         ax_test = fig_test.subplots()
         ar_test = ap.Image(ax_test, self.datasource, origin="lower")
-        ax_test.add_image(ar_test)
+        ax_test.add_artist(ar_test)
         ax_test.set_aspect(1.0)
         fig_test.colorbar(ar_test, ax=ax_test)
         fig_test.canvas.draw()
@@ -47,29 +48,41 @@ class TestArray:
 
     @check_figures_equal(extensions=['png'])
     def test_line(self, fig_test, fig_ref):
-        xhigh, yhigh = self.datasource.data.shape
 
         ax_test= fig_test.subplots()
         artist_test = ap.Line(self.datasource)
         ax_test.add_artist(artist_test)
-
-        ax_test.set_xlim(0, xhigh-1)
-        ax_test.set_ylim(0, yhigh-1)
+        ax_test.set(xlim=(0, self.rows-1), 
+                    ylim=(0, self.cols-1))
         fig_ref.canvas.draw()
 
         ax_ref = fig_ref.subplots()
-        artist_ref = ax_ref.plot(self.datasource.data, color='C0')
-        ax_ref.set_xlim(0, xhigh-1)
-        ax_ref.set_ylim(0, yhigh-1)
+        _ = ax_ref.plot(self.datasource.data, color='C0')
+        ax_ref.set(xlim=(0, self.rows-1), 
+                   ylim=(0, self.cols-1))
         fig_test.canvas.draw()
+        axes_property_check(ax_test, ax_ref)
+    
+    @check_figures_equal(extensions=['png'])
+    def test_bar_grouped(self, fig_test, fig_ref):
+        ax_test = fig_test.subplots()
+        artist_test = ap.Bar(self.datasource)
+        ax_test.add_artist(artist_test)
+        ax_test.set(xlim=(0, self.rows-1), 
+                    ylim=(0, self.cols-1))
+
+        ax_ref = fig_ref.subplots()
+        for i, row in enumerate(self.datasource.data.T):
+            x = np.arange(row.shape[0]) + (i/self.rows)
+            width = 1/self.rows
+            _ = ax_ref.bar(x, row, width)
+        ax_ref.set(xlim=(0, self.rows-1), 
+                   ylim=(0, self.cols-1))
 
         axes_property_check(ax_test, ax_ref)
     
+        
     
-    @pytest.mark.skip()
-    @check_figures_equal(extensions=['png'])
-    def test_bar_grouped(self, fig_test, fig_ref):
-        pass
     
     @pytest.mark.skip()
     @check_figures_equal(extensions='png')
