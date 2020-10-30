76903df077cead91ab71b8274d1ca98e4b4e7545
hannah <story645@gmail.com>
2020-10-30 16:30:54 -0400

    colors

---
+++ code/matplottoy/encoders/color.py
@@ -0,0 +1,24 @@
+class IdentityColor:
+    @staticmethod
+    def convert(value):
+        values = np.atleast_1d(np.array(value, dtype=object))
+        return [mcolors.to_rgba(v) for v in values]
+
+    @staticmethod
+    def validate(mtype):
+        return all(mcolors.is_color_like(v) for v in mtype.categories)
+
+class NominalColor:
+    def __init__(self, mapping):
+        """goal of init is to store parameters that would otherwise be
+        curried higher level function"""
+        self._mapping = mapping
+
+    def convert(self, value):
+        values = np.atleast_1d(np.array(value, dtype=object))
+        return [mcolors.to_rgba(self._mapping[v]) for v in values]
+
+    def validate(self, mtype):
+        return (set(mtype.categories) == self._mapping.keys() and
+        all(mcolors.is_color_like(color) for color in self._mapping.values()))
+
