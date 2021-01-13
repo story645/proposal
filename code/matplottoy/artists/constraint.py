ca1eac3015220ec0eb6f5e1588535275742558b5
hannah <story645@gmail.com>
2021-01-13 01:00:05 -0500

    start of nu constraint check functions + words and words

---
+++ code/matplottoy/artists/constraint.py
@@ -0,0 +1,17 @@
+
+def is_partial_order(values, transform):
+    return True
+
+def is_nominal(values, transform):
+    return True
+
+def is_ordinal(values, transform):
+    return True
+
+def is_interval(values, transform):
+    return True
+
+def is_ratio(values, transform):
+    return True
+
+
