0985fbe7eaac0c448cbdf07b4e23fe9f7fafa190
hannah <story645@gmail.com>
2021-04-26 05:14:08 -0400

    moving around

--- figures/intro_plots.py
+++ figures/intro_plots.py
@@ -14,16 +14,22 @@ cdict = {'GANG MILLS NEW YORK': '#e6194B',
          'SARA NEW YORK': '#42d4f4',
          'SCHROON LAKE NEW YORK': '#9A6324',
          'SHERBURNE NEW YORK': '#4363d8',
-         'STONYKILL NEW YORK': '#469990',
+         'STONYKILL NEW YORK': '#fabed4',
          'BINGHAMTON': '#3cb44b',
          'ISLIP LI MACARTHUR AP': '#ffe119',
          'NEW YORK LAGUARDIA AP': '#f58231',
          'BUFFALO': '#f032e6',
-         'ALBANY AP': '#fabed4',
+         'ALBANY AP':'#469990',
          'GLENS FALLS AP': '#dcbeff',
          'ROCHESTER GTR INTL AP': '#800000',
          'SYRACUSE HANCOCK INTL AP': '#aaffc3',
          'NEW YORK JFK INTL AP': '#000075'}
+
+ssubset = ['ROCHESTER GTR INTL AP','STONYKILL NEW YORK', 
+           'ALBANY AP', 'SCHROON LAKE NEW YORK', 
+           'SARA NEW YORK','NEW YORK LAGUARDIA AP',
+           'NEW YORK JFK INTL AP', 'SYRACUSE HANCOCK INTL AP', 
+           'ISLIP LI MACARTHUR AP', 'GLENS FALLS AP']
 
 def plot_table(ax, dfs, ccolors, edgecolor='k', textcolor='k'):
     tab = mtable.table(ax, cellText=dfs.astype('str').values, cellLoc='center',
@@ -49,17 +55,18 @@ def plot_table(ax, dfs, ccolors, edgecolor='k', textcolor='k'):
 def plot_map(ax, gdf, nystate, cmap, norm, fade='k', alpha=.5, station=None):
     for name in cdict:
         a = 1 if name == station else alpha
-        df = gdf.loc[[name], ['TAVG','geometry']]
-        df.plot(ax=ax, label=name, facecolor=cmap(norm(df['TAVG'])), 
-                                edgecolor=mcolors.to_rgba(cdict[name], alpha=a), legend=False,
-                                linewidth=2.5, alpha=a, markersize=100)
+        df = gdf.loc[[name], ['PRCPI','geometry']]
+        df.plot(ax=ax, label=name, facecolor=cmap(norm(df['PRCPI'])), 
+                edgecolor=mcolors.to_rgba(cdict[name], alpha=a), 
+                legend=False, linewidth=2.5, alpha=a, markersize=100)
     
     nystate[nystate['postal'].str.match('NY')].plot(ax=ax,
-                                facecolor='white', edgecolor=fade, zorder=-1)
+                            facecolor='white', edgecolor=fade, zorder=-1)
     
     ax1_divider = make_axes_locatable(ax)
     cax = ax1_divider.append_axes("right", size="5%", pad="2%")
     cb = mpl.colorbar.ColorbarBase(cax, cmap=cmap,norm=norm, orientation='vertical', drawedges=False, alpha=alpha)
+    cb.set_label('total precipitation (in.)', color=fade)
     for axes in [ax, cax]:
         axes.tick_params(color=fade, labelcolor=fade)
         axes.spines[:].set_color(fade)
@@ -71,10 +78,13 @@ def plot_time(ax, df, fade='k', alpha=.5, station=None):
     for name, gdf in df.groupby(['NAME']):
         a = 1 if name == station else alpha
         zorder = 100 if name == station else -1
-        ax.plot('DATES', 'TAVG', data=gdf, label=name, color=cdict[name], alpha=a, zorder=zorder, lw=2)
+        ax.plot('DATES', 'PRCPI', data=gdf, label=name, color=cdict[name], alpha=a, zorder=zorder, lw=2)
     ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(mdates.AutoDateLocator()))
     ax.tick_params(color=fade, labelcolor=fade)
     ax.spines[:].set_color(fade)
+    ax.set_ylabel('precipitation (in.)', color=fade)
+    ax.yaxis.set_label_position('right')
+    ax.tick_params('y', labelright=True, labelleft=False, right=False, left=False)
     return 
     
 def source_cell(cell, color='k', xr=.5, yr=.5):
