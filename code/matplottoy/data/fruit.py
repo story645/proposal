a5f0a939751eebe82f232c0855603ea6d47d9bf3
hannah <story645@gmail.com>
2021-08-09 16:11:44 -0400

    functional taus and xis

--- code/matplottoy/data/fruit.py
+++ code/matplottoy/data/fruit.py
@@ -22,9 +22,8 @@ class Section:
     def __post_init__(self):
         #check the fiber matches
         assert self.section.dtypes.to_dict() == self.F
-        # in this specific case, is easier to check that they're the same type
+        # checking on type since section doesn't have to be global
         assert isinstance(self.section.index, self.K)
-
 
 
 def fiberbundle(section): #dataframe is section, type annotations
@@ -34,19 +33,19 @@ def fiberbundle(section): #dataframe is section, type annotations
 
     #split c in half to keep to preserve data/visual split, 
     # this is b/c we don't have direct S->K
-    # matplotlib doesn't want to know everyone's indexing method
-    def subset_k(subsets): # inverse nu should happen to the artist - just bounding boxes {fiber, subset in data coordinates}
-        """axes is a restriction on the index, maybe it's xi
-        might be xi, might be the index/axes subsetting
-        index comes from the section/is part of the FB definition 
-        """
-        #define it as filtering is optional
-        index = section.section.index# in theory can subselect here on info from the axes
-        def projection(column_name):
+    # matplotlib doesn't want to know everyone's indexing method  
+    def projection(column_name):
+        def subset_k(subsets): # inverse nu should happen to the artist - just bounding boxes {fiber, subset in data coordinates}
+            """axes is a restriction on the index, maybe it's xi
+            might be xi, might be the index/axes subsetting
+            index comes from the section/is part of the FB definition 
+            """
+            #define it as filtering is optional
+            index = section.section.index# in theory can subselect here on info from the axes
             def values(): #
                 if column_name is None:
                     return pd.Series(index=index, dtype=float)
                 return section.section[column_name][index]
             return values 
-        return projection
-    return subset_k #s
+        return subset_k
+    return projection
