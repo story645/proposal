23993d394dba52e63af1bd78bfd7bb36dfe2b190
hannah <story645@gmail.com>
2021-08-25 13:00:01 -0400

    clean up accidental inclusion

--- leg.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-matplotlib.use('TkAgg')
-
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(figsize=(6,10))
-l, = ax.plot([1,2,3], [1,2,3], label="hello")
-ax.legend(bbox_to_anchor=(1, 1))
-plt.show()
